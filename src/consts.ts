772c530bb5a677285ad0925a5c55b0ad2e0cd4b3
武井彗 <takeisatoshi@takeinoMacBook-Air.local>
2026-05-13 16:18:11 +0900

    favicon,ogp,thumbnail,title,discription update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'さとしのブログ';
+export const SITE_DESCRIPTION = 'Web開発、オタク活動、撮影した写真などを中心に、さとしが日々感じたことを綴るブログです。';
